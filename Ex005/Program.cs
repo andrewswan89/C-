d50a18417fbe2c9964890498471a4614b70f2f22
SWAN <andrewswan89@gmail.com>
2022-05-10 17:21:03 +0700

    ex005 любимчик

---
+++ Ex005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("ВВедите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "миша")
+{
+    Console.WriteLine("Ура! Это же МИША!");
+}
+else
+{
+    Console.Write("Здравствуй, ");
+    Console.WriteLine(username);
+}
