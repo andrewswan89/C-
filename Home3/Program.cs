cdee8997ac510e28b0d8253bc670e2e875c01028
SWAN <andrewswan89@gmail.com>
2022-05-10 19:07:04 +0700

    дз3 определние четности числа

--- Home3/Program.cs
+++ Home3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число: ");
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Введенное число " + a + " четное");
+}
+else
+{
+    Console.WriteLine("Введенное число " + a + " НЕ четное");
+} 
