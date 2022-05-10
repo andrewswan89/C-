142693fae59ebb5730a1555598a15e030a264777
SWAN <andrewswan89@gmail.com>
2022-05-10 19:30:33 +0700

    д3 четные числа от 1 до N

--- Home4/Program.cs
+++ Home4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+//принимает число (N), а на выходе показывает все чётные
+//числа от 1 до N.
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int num = int.Parse(number);
+
+int count = 1;
+
+while(count <= num)
+{
+    if(count % 2 == 0)
+    {
+        Console.Write(count);
+        Console.Write(", ");
+    }
+    count++;
+}
+Console.WriteLine();
