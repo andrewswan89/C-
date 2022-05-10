70db53378ea6299dbd2fb0d292a0dbcb1126c3ce
SWAN <andrewswan89@gmail.com>
2022-05-10 18:51:31 +0700

    дз2 максимальное число из 3х

---
+++ Home2/Program .cs
@@ -0,0 +1,30 @@
+﻿// 2. Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число: ");
+string s_a = Console.ReadLine();
+Console.WriteLine("Введите второе число: ");
+string s_b = Console.ReadLine();
+Console.WriteLine("Введите третье число: ");
+string s_с = Console.ReadLine();
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+int c = int.Parse(s_с);
+
+if (b > a)
+{
+    a = b;
+    if (c > a)
+    {
+        a = c;
+    }
+}
+else
+{
+    if (c > a)
+    {
+        a = c;
+    }
+}
+Console.Write("Число " + a + " максимальное");
