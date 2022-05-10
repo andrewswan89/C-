88092be89090165ae9543cd3f849fa75afebbe88
SWAN <andrewswan89@gmail.com>
2022-05-10 19:41:02 +0700

    д3 модуль числа

--- Home5/Program.cs
+++ Home5/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter number: ");
+string number = Console.ReadLine();
+int num = int.Parse(number);
+
+if(num < 0) num = -num;
+Console.WriteLine(num);
