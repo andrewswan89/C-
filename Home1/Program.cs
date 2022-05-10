246e4284282b0c6771cffda08da1e7633f0f8e27
SWAN <andrewswan89@gmail.com>
2022-05-10 18:19:05 +0700

    дз1 ввод числа

--- Home1/Program.cs
+++ Home1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+string s_a = Console.ReadLine();
+Console.WriteLine("Введите второе число: ");
+string s_b = Console.ReadLine();
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+if (a > b)
+{
+    Console.Write("Число " + a + " больше числа " + b);
+}
+else
+{
+    Console.Write("Число " + b + " больше числа " + a);
+}
