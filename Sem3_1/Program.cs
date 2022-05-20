2e874f07a00ee3be18f9f8f3fdde0aa736dc5fe5
SWAN <andrewswan89@gmail.com>
2022-05-20 22:51:45 +0700

    д3 palindrome

---
+++ Sem3_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+System.Console.WriteLine();
+
+System.Console.Write("Введите целое число:   ");
+string number = Console.ReadLine();
+int length = number.Length;
+string txtToCheck = number.Replace(" ","");  /// для проверки текста на палиндром
+
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+
+System.Console.WriteLine();
+if (IsPalindrome()) System.Console.WriteLine($"1. Введный текст/число - {number} - является палиндромом");
+else System.Console.WriteLine($"1. Число {number} не является палиндромом");
+System.Console.WriteLine();
