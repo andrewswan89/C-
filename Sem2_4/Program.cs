a891b1b7e978f04860509923fbf7ab324e927a2a
SWAN <andrewswan89@gmail.com>
2022-05-20 22:26:32 +0700

    д3 weekend or not

---
+++ Sem2_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// 4. Напишите программу, которая принимает на вход цифру,
+//    обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void DayNumber(int num)
+{
+    if(num >=1 & num <= 5)
+    {
+        Console.WriteLine(" - Рабочий день");
+    }
+    if(num >=6 & num <= 7)
+    {
+        Console.WriteLine(" - Выходной день");
+    }
+}
+
+Console.WriteLine("Введите цифру дня недели");
+string id=Console.ReadLine();
+int idw = int.Parse(id);
+DayNumber(idw);
