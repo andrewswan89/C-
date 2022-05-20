64a5e1ddf9accb9e69f2ad3f8eb2ca942a3aaf85
SWAN <andrewswan89@gmail.com>
2022-05-20 22:00:28 +0700

    д3 second numb

---
+++ Sem2_1/Program.cs
@@ -0,0 +1,10 @@
+﻿int TNum(int num)
+{
+    int sc = 0;
+    sc = (num % 100 - num % 10) / 10;
+    return sc;
+}
+Console.WriteLine("Введите трехзначное число");
+string isn=Console.ReadLine();
+int isns = int.Parse(isn);
+Console.WriteLine($"Вторая цифра этого числа {TNum(isns)}");
