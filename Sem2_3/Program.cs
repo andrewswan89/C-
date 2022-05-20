4cd8f7e3ae8ff01a90cc8fa9f793d907e442dc58
SWAN <andrewswan89@gmail.com>
2022-05-20 22:17:57 +0700

    д3 second and third numb

---
+++ Sem2_3/Program.cs
@@ -0,0 +1,17 @@
+﻿int TNum(int num)
+{
+    int sc = 0;
+    sc = num % 10;
+    return sc;
+}
+Console.WriteLine("Введите число");
+string isn=Console.ReadLine();
+int ist = int.Parse(isn);
+if(ist<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(TNum(ist));  
+}
