3845bbb5d791e1c2362b643f3e2dfb80bbeb33b8
SWAN <andrewswan89@gmail.com>
2022-05-27 23:01:51 +0700

    dz count number

---
+++ Sem4_2/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.Write("Введите число:   ");
+string writeNum = Console.ReadLine();
+int number = Convert.ToInt32(writeNum);
+int result = number;
+
+// первый способ
+
+int count1 = 0;
+while (result > 0)
+{
+    result = result / 10;
+    count1++;
+}
+System.Console.WriteLine();
+System.Console.WriteLine($"В числе {number} {count1} цифр");
+System.Console.WriteLine();
