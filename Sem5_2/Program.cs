fecbfd678581f53801fae6e57f148aa5d0d34818
SWAN <andrewswan89@gmail.com>
2022-05-27 23:27:05 +0700

    dz  array count even uneven

---
+++ Sem5_2/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+int size = 20;
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintResult(int[] array)
+{
+    int even = 0; //четные
+    int uneven = 0;// нечетные 
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+        if (array[i] % 2 == 0) even++;
+        else uneven++; // нечетные числа
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Количество четных чисел {even}, количество нечетных чисел {uneven}");
+    System.Console.WriteLine();
+}
+
+PrintResult(FillArray(size));
