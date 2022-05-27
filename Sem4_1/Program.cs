879a07a2b392f314880eab856f9b3a307f903061
SWAN <andrewswan89@gmail.com>
2022-05-27 22:16:19 +0700

    Sem4_1

---
+++ Sem4_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1. Напишите цикл, который принимает на вход два числа (A и B)
+//    и возводит число A в натуральную степень B.
+
+int Exponentiation(int a, int b)
+{
+    int all =a;
+    for(int i=1; i<b; i++)
+    {
+        all *= a;
+        
+    }
+    return all;
+}
+Console.WriteLine(Exponentiation(3,5));
+Console.WriteLine(Exponentiation(2,4));
+Console.WriteLine(Exponentiation(2,5));
